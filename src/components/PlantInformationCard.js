23360bd7a01a81fafdaa733aa652a63846c273ad
Duy Truong <41127922+duyhtruong@users.noreply.github.com>
2021-03-21 22:57:58 -0700

    Add files via upload

--- src/components/PlantInformationCard.js
+++ src/components/PlantInformationCard.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import {Card} from 'react-bootstrap';
+import plant_placeholder from '../assets/plant-placeholder.jpg';
 
-class PlantInformationCard extends React.Component{
-    render(){
-        return(
-            <div>Card </div>
+class PlantInformationCard extends React.Component {
+    render() {
+        return (
+            <Card>
+                <Card.Img variant="top" src={plant_placeholder} />
+                <Card.Body>
+                    <Card.Title>Card title</Card.Title>
+                    <Card.Text>
+                        This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                </Card.Footer>
+            </Card>
         );
     }
 }
--- src/components/PlantSearchContainer.js
+++ src/components/PlantSearchContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PlantSearchBar from './PlantSearchBar';
 import PlantInformationCard from './PlantInformationCard';
 
-import {Container, Col, Row} from 'react-bootstrap';
+import {Container, Col, Row, CardDeck} from 'react-bootstrap';
 
 class PlantSearchContainer extends React.Component{
     render(){
@@ -14,16 +14,16 @@ class PlantSearchContainer extends React.Component{
                 <PlantSearchBar />
                 </Col>
             </Row>
-            <Row>
-                <Col><PlantInformationCard /></Col>
-                <Col><PlantInformationCard /></Col>
-                <Col><PlantInformationCard /></Col>
-            </Row>
-            <Row>
-                <Col><PlantInformationCard /></Col>
-                <Col><PlantInformationCard /></Col>
-                <Col><PlantInformationCard /></Col>
-            </Row>
+            <CardDeck className="my-4">
+                <PlantInformationCard />
+                <PlantInformationCard />
+                <PlantInformationCard />
+            </CardDeck>
+            <CardDeck>
+                <PlantInformationCard />
+                <PlantInformationCard />
+                <PlantInformationCard />
+            </CardDeck>
             </Container>
             
         )
