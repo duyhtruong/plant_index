32960b071c01f73ff63eb6fa35e9c3695b6b002f
Duy Truong <41127922+duyhtruong@users.noreply.github.com>
2021-03-22 00:18:23 -0700

    Add files via upload

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,14 +1,46 @@
-import React from 'react';
-
-import {Nav, Navbar} from 'react-bootstrap';
+import React, {useState} from 'react';
+import {Nav, Navbar, Button, Modal} from 'react-bootstrap';
+import LoginModal from './LoginModal';
 
 class Navigation extends React.Component {
     render() {
+
+        function LoginModal(){ 
+            const [show, setShow] = useState(false);
+        
+            const handleClose = () => setShow(false);
+            const handleShow = () => setShow(true);
+        
+            return (
+            <>
+              <Button variant="primary" onClick={handleShow}>
+                Launch demo modal
+              </Button>
+        
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Modal heading</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                  <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+              </>
+          );
+        
+        }
+
         return (
 
 
             <div>
-
+               
                 <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
                     <Navbar.Brand href="#home">Plant Index</Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
@@ -20,6 +52,7 @@ class Navigation extends React.Component {
                         <Nav>
                             <Nav.Link href="#deets">Search Plants</Nav.Link>
                             <Nav.Link href="#deets">Buy Plants</Nav.Link>
+                            <Nav.Link ><LoginModal/></Nav.Link>
                             <Nav.Link href="#deets">My Plants</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
